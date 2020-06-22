4a6aa443347b361d25acef60b3e0ddf96681d6e4
swirkes <shane.wirkes@edu.turkuamk.fi>
2020-06-22 11:38:11 +0300

    second commit

---
+++ Navigation/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Navigation
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new WelcomePage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
